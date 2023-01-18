5f37394af8e89a63c12acf47d46cfc217c736760
amberwagoner <amberwagoner@outlook.com>
2023-01-18 00:36:39 +0000

    Tasks 0-2

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Define square"""
+    def __init__(self, size=0):
+        """Initialize square with a size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Prints area"""
+        return self.__size ** 2
