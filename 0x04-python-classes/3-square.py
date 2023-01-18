78c837847d143d29a7543438c3f392e31faa6f03
amberwagoner <amberwagoner@outlook.com>
2023-01-18 00:58:23 +0000

    Task 3

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a square"""
+
+
+class Square:
+    """Define square"""
+    def __init__(self, size=0):
+        """Initializes a square with a size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Prints area"""
+        return self.__size
+
+    @property
+    def size(self):
+        """Gets a square's size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets a square's size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
