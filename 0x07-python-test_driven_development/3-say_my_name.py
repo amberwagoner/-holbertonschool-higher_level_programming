9f09e81ab9fcbefc5dd69ebf3129a693c03f43ff
amberwagoner <amberwagoner@outlook.com>
2023-01-25 15:21:01 -0800

    Initial push

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 3-say_my_name.py """
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints 'My name is <first name> <last name>'"""
+
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
