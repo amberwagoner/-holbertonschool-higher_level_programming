f02b96f0c5b00fe10cf63c795c35b42fa38880fd
amberwagoner <amberwagoner@outlook.com>
2023-01-27 13:27:48 -0800

    Task 2 & 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class and inherited class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Checks if an object is an instance of a class or inherited class"""
+    return isinstance(obj, a_class)
+    
