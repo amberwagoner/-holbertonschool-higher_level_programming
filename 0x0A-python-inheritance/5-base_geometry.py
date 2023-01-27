6b459f381a1d7af046c7a40d11b4ba8e95a62e6c
amberwagoner <amberwagoner@outlook.com>
2023-01-27 15:39:18 -0800

    Tasks 5 & 6

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Function to create an empty class """
+
+
+class BaseGeometry:
+    """ Empty class """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" Function to create a class """
+
+class BaseGeometry:
+    """ Creates a class with defined area """
+    def area(self):
+        raise Exception("area() is not implemented")
