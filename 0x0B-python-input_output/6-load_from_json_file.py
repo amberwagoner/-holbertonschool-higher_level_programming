6ba51d6a1d48454bd0bb7acdc8ce2d5148ec2849
amberwagoner <amberwagoner@outlook.com>
2023-01-31 12:58:46 -0800

    Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Function that creates an object """
+import json
+
+
+def load_from_json_file(filename):
+    """ Creates an object from a JSON file """
+
+    with open(filename) as f:
+        return json.load(f)
