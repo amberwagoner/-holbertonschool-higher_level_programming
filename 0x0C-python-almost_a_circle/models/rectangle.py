73bc1f0b8757c1bc3adcb8aafac970b738363201
amberwagoner <amberwagoner@outlook.com>
2023-02-01 15:13:37 -0800

    Task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,3 +13,23 @@ class Rectangle(base):
         self.height = height
         self.x = x
         self.y = y
+
+    @property
+    def width(self):
+        """int: Assigns value to width"""
+        return self.__width
+
+    @property
+    def height(self):
+        """int: Assigns value to height"""
+        return self.__height
+
+    @property
+    def x(self):
+        """int: Assigns value to x"""
+        return self.__x
+
+    @property
+    def y(self):
+        """int: Assigns value to y"""
+        return self.__y
