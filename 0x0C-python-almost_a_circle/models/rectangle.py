667d223d398231cd930bef1e9dd30afbca8e1598
amberwagoner <amberwagoner@outlook.com>
2023-02-01 15:44:15 -0800

    Base not base line 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,7 +3,7 @@
 from models.base import Base
 
 
-class Rectangle(base):
+class Rectangle(Base):
     """ Inherits from Base """
     def __init__(self, width, height, x=0, y=0, id=None):
         """ Initializes class """
