881694ee827f9d9b2b38665d66998fa79ac12fd6
amberwagoner <amberwagoner@outlook.com>
2023-02-01 19:34:19 -0800

    Task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -34,3 +34,11 @@ class Base:
             else:
                 list_dicts = [o.to_dictionary() for o in list_objs]
                 jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns list of JSON string representation """
+        if json_string == "[]" or json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
