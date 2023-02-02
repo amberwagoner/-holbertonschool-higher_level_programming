0a953226a6ccbc1f64c6bf1198496ba6d6518ef7
amberwagoner <amberwagoner@outlook.com>
2023-02-01 19:18:04 -0800

    added staticmethod

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,8 +17,9 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ Returns JSON string of list_dictionaries """
-        if list_dictionaries is None or not list_dictionaries:
+        if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
