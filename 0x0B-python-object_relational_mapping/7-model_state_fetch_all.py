7ac87681f26386d96c2beebae7d53f05ba5a857a
amberwagoner <amberwagoner@outlook.com>
2023-03-01 09:47:23 -0800

    pycodestyle

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,12 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysql.db://{}:{}@localhost:3306/{}".format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(user, pwd, db), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
