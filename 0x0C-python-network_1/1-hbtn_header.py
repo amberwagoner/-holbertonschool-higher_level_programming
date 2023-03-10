794985f8b7bc7beaf0c817bc97e6ebe2ef8a540c
amberwagoner <amberwagoner@outlook.com>
2023-03-10 22:51:01 +0000

    simplified with statement

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """ Displays the X-Request-Id variable """
-import urllib
 import sys
+from sys import argv
+import urllib.request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.headers["X-Request-Id"])
