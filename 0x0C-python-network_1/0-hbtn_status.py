124f4be7c25eaa104018384e86f44a7cf571a7ac
amberwagoner <amberwagoner@outlook.com>
2023-03-10 22:13:32 +0000

    simplified code

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -3,21 +3,13 @@
 import urllib.request
 
 
-def fetch_status_page(url):
-    """ Fetches that URL! """
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        content = body.decode('utf-8')
-    return {
-        'type': type(body),
-        'content': body,
-        'utf8_content': content
-    }
+url = 'https://intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    response_info = fetch_status_page(url)
-    print("Body response:")
-    print("    - type: {}".format(response_info['type']))
-    print("    - content: {}".format(response_info['content']))
-    print("    - utf8 content: {}".format(response_info['utf8_content']))
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    content = body.decode('utf-8')
+
+print("Body response:")
+print("    - type: {}".format(type(body)))
+print("    - content: {}".format(body))
+print("    - utf8 content: {}".format(content))
