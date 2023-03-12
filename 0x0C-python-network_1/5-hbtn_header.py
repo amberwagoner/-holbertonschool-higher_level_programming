507045d729c0dc73679938bc1d763e03b7b9d914
amberwagoner <amberwagoner@outlook.com>
2023-03-12 20:06:11 +0000

    so over this task

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Takes in URL, sends in request, displays value of variable """
 import requests
-from sys import argv
+import sys
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(requests.get(sys.argv[1]).headers.get('X-Request-Id'))
