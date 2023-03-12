64307c42591913d7a294034b0ab00f7e77a69974
amberwagoner <amberwagoner@outlook.com>
2023-03-12 19:51:16 +0000

    reformatting

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Takes in URL, sends in request, displays value of variable """
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == '__main__':
-    response = requests.get(argv[1])
-    print(response.headers.get('X-Request-Id'))
+    print(requests.get(sys.argv[1]).headers.get('X-Request-Id'))
