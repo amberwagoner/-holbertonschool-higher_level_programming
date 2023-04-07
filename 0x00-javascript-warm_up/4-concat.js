dec4492a33b6dd045f744be884ba120cf1d901a9
amberwagoner <amberwagoner@outlook.com>
2023-04-06 20:52:52 -0700

    simplified statement

--- 0x00-javascript-warm_up/4-concat.js
+++ 0x00-javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const [arg1, arg2] = process.argv.slice(2);
 
-if (!arg1 || !arg2) {
-  console.log('Please provide two arguments');
-} else {
-  console.log(`${arg1} is ${arg2}`);
-}
+console.log(`${arg1} is ${arg2}`);
