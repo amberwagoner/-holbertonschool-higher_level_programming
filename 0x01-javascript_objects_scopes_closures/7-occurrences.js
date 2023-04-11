d92802f68e709e3e930105fafa0516fb162d927c
amberwagoner <amberwagoner@outlook.com>
2023-04-10 17:59:55 -0700

    typo line 6

--- 0x01-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x01-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
-  for (let i = 0; i < list.lenth; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       count++;
     }
