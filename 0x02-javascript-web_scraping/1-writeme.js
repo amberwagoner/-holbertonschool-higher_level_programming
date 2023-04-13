6738163485b864368d772f520ecf41f077f5912f
amberwagoner <amberwagoner@outlook.com>
2023-04-13 13:15:33 -0700

    task 1

---
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// Task 1
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+  console.error('arg error');
+} else {
+  const filePath = process.argv[2];
+  const contentToWrite = process.argv[3];
+
+  fs.writeFile(filePath, contentToWrite, 'utf-8', (error) => {
+    if (error) {
+      console.error(`Error occurred while writing to the file: ${error}`);
+      return;
+    }
+
+    console.log(`Successfully wrote "${contentToWrite}" to file "${filePath}"`);
+  });
+}
