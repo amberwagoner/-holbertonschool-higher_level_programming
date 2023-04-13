9f869fe271f099b3e3c8d18ec5c0cdf3db7c3366
amberwagoner <amberwagoner@outlook.com>
2023-04-13 14:03:15 -0700

    task 5

---
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// Task 5
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length < 4) {
+  console.error('URL needed');
+} else {
+  const url = process.argv[2];
+  const filePath = process.argv[3];
+
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(`Error occurred while making the GET request: ${error}`);
+      return;
+    }
+
+    fs.writeFile(filePath, body, 'utf8', (error) => {
+      if (error) {
+        console.error(`Error occurred while writing to the file: ${error}`);
+      } else {
+        console.log(`Successfully wrote response to ${filePath}`);
+      }
+    });
+  });
+}
