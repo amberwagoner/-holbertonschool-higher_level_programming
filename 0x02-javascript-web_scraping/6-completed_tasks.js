fe2c1af00f8954d81310b211658d8159cb90dcf1
amberwagoner <amberwagoner@outlook.com>
2023-04-14 14:02:38 -0700

    over it

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -1,36 +1,24 @@
 #!/usr/bin/node
 // Task 6
 const request = require('request');
-
-const api_url = "https://jsonplaceholder.typicode.com/todos";
-
-request(api_url, function (error, response, body) {
+const website = process.argv[2];
+request(website, function (error, response, body) {
   if (error) {
-    console.error("Error: Failed to fetch data from API");
-    return;
-  }
-  
-  if (response.statusCode !== 200) {
-    console.error("Error: Failed to fetch data from API");
-    return;
-  }
-  
-  const data = JSON.parse(body);
-  
-  const completedTasksByUser = {};
-  
-  for (const task of data) {
-    if (task.completed) {
-      const userId = task.userId;
-      if (userId in completedTasksByUser) {
-        completedTasksByUser[userId]++;
-      } else {
-        completedTasksByUser[userId] = 1;
+    console.error(error);
+  } else {
+    const completedTask = {};
+    const content = JSON.parse(body);
+    for (let i = 0; i < content.length; i++) {
+      const id = content[i].userId;
+      const completed = content[i].completed;
+      if (completed) {
+        if (!completedTask[id]) {
+          completedTask[content[i].userId] = 1;
+        } else {
+          completedTask[content[i].userId] += 1;
+        }
       }
     }
-  }
-
-  for (const [userId, completedTaskCount] of Object.entries(completedTasksByUser)) {
-    console.log(`'${userId}': ${completedTaskCount}`);
+    console.log(completedTask);
   }
 });
