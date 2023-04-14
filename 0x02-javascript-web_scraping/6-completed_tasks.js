43a6f1c48ed5a8c1e6123798bbf119df91c29adc
amberwagoner <amberwagoner@outlook.com>
2023-04-14 13:50:15 -0700

    starting fresh

--- 0x02-javascript-web_scraping/6-completed_tasks.js
+++ 0x02-javascript-web_scraping/6-completed_tasks.js
@@ -2,36 +2,35 @@
 // Task 6
 const request = require('request');
 
-if (process.argv.length < 3) {
-  console.error('URL needed');
-} else {
-  const apiUrl = process.argv[2];
+const api_url = "https://jsonplaceholder.typicode.com/todos";
 
-  request.get(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(`Error occurred while making the GET request: ${error}`);
-      return;
+request(api_url, function (error, response, body) {
+  if (error) {
+    console.error("Error: Failed to fetch data from API");
+    return;
+  }
+  
+  if (response.statusCode !== 200) {
+    console.error("Error: Failed to fetch data from API");
+    return;
+  }
+  
+  const data = JSON.parse(body);
+  
+  const completedTasksByUser = {};
+  
+  for (const task of data) {
+    if (task.completed) {
+      const userId = task.userId;
+      if (userId in completedTasksByUser) {
+        completedTasksByUser[userId]++;
+      } else {
+        completedTasksByUser[userId] = 1;
+      }
     }
+  }
 
-    if (response.statusCode === 200) {
-      const todos = JSON.parse(body);
-      const userTaskCounts = {};
-
-      todos.forEach((todo) => {
-        if (todo.completed) {
-          if (userTaskCounts[todo.userId]) {
-            userTaskCounts[todo.userId] += 1;
-          } else {
-            userTaskCounts[todo.userId] = 1;
-          }
-        }
-      });
-
-      Object.keys(userTaskCounts).forEach(userId => {
-        console.log(`${userId}: ${userTaskCounts[userId]}`);
-      });
-    } else {
-      console.error(`Error occurred while fetching the todos: ${response.statusCode}`);
-    }
-  });
-}
+  for (const [userId, completedTaskCount] of Object.entries(completedTasksByUser)) {
+    console.log(`'${userId}': ${completedTaskCount}`);
+  }
+});
